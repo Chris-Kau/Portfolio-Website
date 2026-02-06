97cc13bbd28c1dc7964b55e966a2b69392e46e96
uh chris <116903385+Chris-Kau@users.noreply.github.com>
2026-02-05 16:29:48 -0800

    why theme no work?

---
+++ src/assets/Navbar.vue
@@ -0,0 +1,8 @@
+<template>
+  <v-app-bar :elevation="2" rounded color="background">
+    <template v-slot:prepend>
+      <v-icon icon="$vuetify"></v-icon>
+    </template>
+    <v-app-bar-title>Application Bar</v-app-bar-title>
+  </v-app-bar>
+</template>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,27 @@
+
 import { createApp } from 'vue'
 import App from './App.vue'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 import "../src/assets/main.css"
 
-createApp(App).mount('#app')
+const oshawottTheme = {
+    dark: false,
+    colors:{
+        'primary': "#4C5273",
+        'secondary': "#84BFB9",
+        'accent': "#F2E1AC",
+        'supportingaccent': "#A67232",
+        'text': "#0D0D0D",
+        'background': "#F3F5F0",
+    }
+}
+const vuetify = createVuetify({
+  components,
+  directives,
+  themes: {oshawottTheme},
+})
+
+createApp(App).use(vuetify).mount('#app')
